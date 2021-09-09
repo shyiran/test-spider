46bc20bab463f50200f43d903cab703314c71896
shyiran <shyiran123@163.com>
2021-09-09 17:17:32 +0800

    编码转换

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module test-spider
 
 go 1.16
+
+require (
+	golang.org/x/net v0.0.0-20210908191846-a5e095526f91 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,23 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
-func main()  {
-	respon,err:=http.Get("https://book.douban.com/")
+func main() {
+	//respon, err := http.Get("https://book.douban.com/")//
+	respon, err := http.Get("http://www.chinanews.com/")//utf编码
 	if err != nil {
 		panic(err)
 	}
 	defer respon.Body.Close()
-	if respon.StatusCode != http.StatusOK{
-		fmt.Sprintf("error status code",respon.StatusCode)
+	if respon.StatusCode != http.StatusOK {
+		fmt.Sprintf("error status code", respon.StatusCode)
 	}
-	res,err:=ioutil.ReadAll(respon.Body)
+	bodyReader := bufio.NewReader(respon.Body)
+	e := determinEncodeing(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	res, err := ioutil.ReadAll(utf8Reader)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%s",res)
+	fmt.Printf("%s", res)
+}
+
+func determinEncodeing(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+	if err != nil {
+		log.Printf("获取失败:%v", err)
+		return unicode.UTF8
+	}
+	re,_,_ := charset.DetermineEncoding(bytes, "")
+	return re
 }
